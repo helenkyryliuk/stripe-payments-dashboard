4a515be663842b6ae5cc7ef6803b5698e312d339
helenkyryliuk <helen.kyryliuk@gmail.com>
2026-05-24 16:44:12 +1200

    install dotenv and add env variable

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import ViteExpress from "vite-express";
-
+import dotenv from "dotenv";
 const app = express();
+
+dotenv.config();
 
 app.get("/api/message", (_, res) => res.send("Hello from Express!"));
 
